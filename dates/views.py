f5be36cbf6c6033379df78bde0c47ae527430ea7
Luis Carlos <jinchuika@gmail.com>
2017-04-26 16:59:18 -0600

    Filtros api

--- dates/views.py
+++ dates/views.py
@@ -3,14 +3,23 @@ from django.views.generic import View
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListAPIView
 
 from .models import Nahual
 from .serializers import NahualSerializer
+from datetime import date
 
 
+class NahualListView(ListAPIView):
+    serializer_class = NahualSerializer
 
-class NahualListView(APIView):
-	def get(self, request):
-		nahuales = Nahual.objects.all()
-		serializer = NahualSerializer(nahuales, many=True)
-		return Response(serializer.data)
+    def get_queryset(self):
+        d0 = date(1900, 1, 1)
+        d1 = date(
+            int(self.request.query_params.get('anno')), 
+            int(self.request.query_params.get('mes')), 
+            int(self.request.query_params.get('dia')))
+        delta = d1 - d0
+        count = delta.days + 1
+        nahual = count % 20
+        return Nahual.objects.filter(id=nahual)
